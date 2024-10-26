684983014e0313ee90edf74863afd01975202767
“tomoki33” <tomozawa1997@gmail.com>
2024-10-26 11:38:00 +0900

    urls.py構成変更

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter # type: ignore
+from apps.training.views import TrainingViewSet
 
+router = DefaultRouter()
+router.register(r'', TrainingViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('training/', include('apps.training.urls')),
+    path('training/', include(router.urls)),  
   ]
