f4656c1c5e06da061d4596994a710577663f8c9c
“tomoki33” <tomozawa1997@gmail.com>
2025-01-03 08:39:14 +0900

    途中PUSH

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 class User(models.Model):
-    weight = models.IntegerField()
-    height = models.IntegerField()
-    goal = models.CharField(max_length=100)
-    title = models.CharField(max_length=100)
+    email = models.CharField(max_length=100,blank=True)
+    password = models.CharField(max_length=100,blank=True)
+    weight = models.IntegerField(blank=True, null=True)
+    height = models.IntegerField(blank=True, null=True)
+    goal = models.CharField(max_length=100,blank=True)
+    title = models.CharField(max_length=100,blank=True)
 
     def __str__(self):
         return self.title
--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,3 +130,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # フロントエンドのURL
+]
